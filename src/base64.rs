7658030ca8827dd669f3072e0cb42f6f3d7780bc
jyuch <jyuch@users.noreply.github.com>
2023-03-26 15:59:09 +0900

    Update dependencies

--- src/base64.rs
+++ src/base64.rs
@@ -1,3 +1,4 @@
+use base64::engine::general_purpose;
 use std::error::Error;
 use std::fs::File;
 use std::io::copy;
@@ -7,7 +8,7 @@ pub fn encode(input: &Path, output: &Path) -> Result<(), Box<dyn Error>> {
     let mut input = File::open(input)?;
     let mut output = File::create(output)?;
 
-    let mut encoder = base64::write::EncoderWriter::new(&mut output, base64::STANDARD);
+    let mut encoder = base64::write::EncoderWriter::new(&mut output, &general_purpose::STANDARD);
     let _ = copy(&mut input, &mut encoder)?;
 
     Ok(())
@@ -17,7 +18,7 @@ pub fn decode(input: &Path, output: &Path) -> Result<(), Box<dyn Error>> {
     let mut input = File::open(input)?;
     let mut output = File::create(output)?;
 
-    let mut decoder = base64::read::DecoderReader::new(&mut input, base64::STANDARD);
+    let mut decoder = base64::read::DecoderReader::new(&mut input, &general_purpose::STANDARD);
     let _ = copy(&mut decoder, &mut output)?;
 
     Ok(())
